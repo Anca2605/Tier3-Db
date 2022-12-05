0635bb13b2e4cfad166f6d9853cad9ef64ccb30a
Ancaelena2605 <112935366+Ancaelena2605@users.noreply.github.com>
2022-12-05 15:49:49 +0100

    some changes to the methods i added 2 more methods in manager and 2 or 3 in the client i made the bill class

---
+++ Tier3-Db/DataAccess/DataBaseContext.cs
@@ -0,0 +1,46 @@
+﻿using Db3.Models.Client;
+using Db3.Models.Manager;
+using Microsoft.EntityFrameworkCore;
+using Tier3.Models;
+using Tier3.Models.Burial;
+using Tier3.Models.Client;
+using Tier3.Models.Employee;
+using Tier3.Models.Preference;
+
+namespace Tier3.DataAccess
+{
+    public class DataBaseContext : DbContext
+    {
+
+        public DbSet<Manager> Manager{ get; set; }
+        public DbSet<Client> Clients { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(
+                @"Data Source = C:\Users\abadoi\Documents\GitHub\SEP3\Tier3\Tier3\Tier3.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Client>()
+                .HasKey(cb => new
+                {
+                    cb.ClientId
+                });
+
+            modelBuilder.Entity<Client>()
+                .HasOne(cb => cb.Client)
+                .WithMany(cb => cb.Bills)
+                .HasForeignKey(cb => cb.ClientId);
+            
+            modelBuilder.Entity<Manager>()
+                .HasOne<Manager>(b => b.Username)
+                .WithMany(b => b.clients)
+                .HasForeignKey(b => b.ManagerId);
+
+        }
+    }
+}
---
+++ Tier3-Db/Migrations/20211213105110_Final.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Tier3.Migrations
+{
+    public partial class Final : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Username = table.Column<string>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Password = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: true),
+                    Phonenumber = table.Column<string>(type: "TEXT",nullable:true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Manager",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Username = table.Column<string>(type: "TEXT", nullable: false),
+                    Password = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Manager", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Bill",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Provider = table.Column<string>(type: "TEXT", nullable: false),
+                    Date = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ClientId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bill", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Bill_Clients_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Clients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Bills",
+                columns: table => new
+                {
+                    ClientId = table.Column<int>(type: "INTEGER", nullable: false),
+                    BillId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bills", x => new { x.ClientId, x.BillId });
+                    table.ForeignKey(
+                        name: "FK_Bills_Bill_BillId",
+                        column: x => x.BillId,
+                        principalTable: "Bill",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Bills_Clients_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Clients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bill_ClientId",
+                table: "Bill",
+                column: "ClientId");
+            
+            migrationBuilder.CreateIndex(
+                name: "IX_Bills_BillId",
+                table: "Bills",
+                column: "BillId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+            migrationBuilder.DropTable(
+                name: "Bills");
+
+            migrationBuilder.DropTable(
+                name: "Bill");
+
+            migrationBuilder.DropTable(
+                name: "Clients");
+        }
+    }
+}
