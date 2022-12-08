7ed8c2ff105580106ddb05dee2b7c818aa2eb933
BekoLolek <bekololek@gmail.com>
2022-12-08 16:44:31 +0100

    adding api

---
+++ Tier3/Network/Manager/ServerManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using Db3.Repositories.Manager;
+
+namespace Db3.Networking.Manager;
+
+public class ServerManager
+{
+    private Models.Manager.Manager manager;
+    private IManagerRepo managerRepo;
+
+    public ServerManager()
+    {
+        manager = new Models.Manager.Manager();
+        managerRepo = new ManagerRepo();
+    }
+
+    public async void GetManagerById(NetworkStream stream, string content)
+    {
+        Models.Manager.Manager test = JsonSerializer.Deserialize<Models.Manager.Manager>(content);
+        string username = test.Username;
+        string password = test.Password;
+
+        manager = await managerRepo.GetManagerById(int.Parse(content));
+        string reply = JsonSerializer.Serialize(manager);
+        byte[] bytesWrite = Encoding.ASCII.GetBytes(reply);
+        stream.Write(bytesWrite, 0, bytesWrite.Length);
+    }
+    
+    public async void AddClient(NetworkStream stream, string content)
+    {
+        Models.Client.Client test = JsonSerializer.Deserialize<Models.Client.Client>(content);
+        string username = test.Username;
+        string password = test.Password;
+        string name = test.Name;
+        string dob = test.dob;
+        string email = test.Email;
+        string phonenumber = test.phonenumber;
+        int Id = test.Id;
+        Models.Client.Client cl = new Models.Client.Client(Id,username,name,password,email,dob,phonenumber);
+        await managerRepo.AddClient(cl);
+        string reply = JsonSerializer.Serialize(manager);
+        byte[] bytesWrite = Encoding.ASCII.GetBytes(reply);
+        stream.Write(bytesWrite, 0, bytesWrite.Length);
+    }
+    
+    public async void DeleteClient(NetworkStream stream, string content)
+    {
+        Models.Client.Client test = JsonSerializer.Deserialize<Models.Client.Client>(content);
+        string username = test.Username;
+        string password = test.Password;
+
+        await managerRepo.DeleteClient(int.Parse(content));
+
+        string reply = JsonSerializer.Serialize(manager);
+        byte[] bytesWrite = Encoding.ASCII.GetBytes(reply);
+        stream.Write(bytesWrite, 0, bytesWrite.Length);
+    }
+    
+    public async void EditClient(NetworkStream stream, string content)
+    {
+        Models.Client.Client test = JsonSerializer.Deserialize<Models.Client.Client>(content);
+        string username = test.Username;
+        string password = test.Password;
+        string dob = test.dob;
+        string email = test.Email;
+        string phonenumber = test.phonenumber;
+        int Id = test.Id;
+        await managerRepo.GetClient(Id);
+        string reply = JsonSerializer.Serialize(manager);
+        byte[] bytesWrite = Encoding.ASCII.GetBytes(reply);
+        stream.Write(bytesWrite, 0, bytesWrite.Length);
+    }
+
+    public async void DeleteManager(string content)
+    {
+        await managerRepo.DeleteManager(Int32.Parse(content));
+    }
+        
+    public async void GetManagerByUsername(NetworkStream stream, string content)
+    {
+        Models.Manager.Manager manager1 = JsonSerializer.Deserialize<Models.Manager.Manager>(content);
+        string username = manager1.Username;
+        manager = await managerRepo.GetManagerByUsername(username);
+        string reply = JsonSerializer.Serialize(manager);
+        byte[] bytesWrite = Encoding.ASCII.GetBytes(reply);
+        stream.Write(bytesWrite, 0, bytesWrite.Length);
+    }
+}
---
+++ Tier3/Repositories/Client/ClientRepo.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections.ObjectModel;
+using Db3.Models.Client;
+using Db3.Repositories.Client;
+using Microsoft.EntityFrameworkCore;
+
+namespace Tier3_Db.Models
+{
+
+    public class ClientRepo : IClientRepo
+    {
+        private readonly UserContext _context;
+        private DbContext dbCtx;
+        private Client _client;
+
+        async Task<string> IClientRepo.CreateClientAccount(Client _client)
+        {
+            await using (dbCtx = new DbContext())
+            {
+                foreach (var variable in dbCtx.)
+                {
+                    if (variable.Username.Equals(client.Username))
+                    {
+                        Console.WriteLine("Account already exists");
+                        return "Account already exists";
+                    }
+                }
+
+                client.Client = new List<Models.Client.Client>();
+                await dbCtx.Clients.AddAsync(client);
+                Console.WriteLine("Account created");
+                await dbCtx.SaveChangesAsync();
+            }
+
+            return "Account created";
+        }
+
+        public Task<string> CreateClientAccount(Client client)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Client> IClientRepo.GetClient(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Models.Client.Client> GetClient(string username, string password)
+        {
+            try
+            {
+                await using (dbCtx = new DbContext())
+                {
+                    return dbCtx.Clients
+                        .First(c => c.Username.Equals(username) && c.Password.Equals(password));
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Account with username " + username + " not found");
+            }
+        }
+
+        public async Task DeleteClient(int clientId)
+        {
+            await using (dbCtx = new DbContext())
+            {
+                Models.Client.Client client = await dbCtx.Clients
+                    .Include(c => c.Burials)
+                    .ThenInclude(c => c.BurialPreferences)
+                    .Include(c => c.Email)
+                    .Include(c => c.Name)
+                    .Include(c => c.Username)
+                    .Include(c => c.Password)
+                    .FirstAsync(cl => cl.Id == clientId);
+
+                client.Client = new Collection<Models.Client.Client>();
+                dbCtx.Clients.Remove(client);
+                await dbCtx.SaveChangesAsync();
+            }
+        }
+
+        Task<Client> IClientRepo.GetClientByUsername(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Client> IClientRepo.GetClientById(int clientId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Bill.Bill> GetBillById(int parse)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Models.Client.Client> GetClientByUsername(string username)
+        {
+            await using (dbCtx = new DbContext())
+            {
+                Models.Client.Client client;
+
+                try
+                {
+                    client = await dbCtx.Clients
+                        .Include(cl => cl.Burials)
+                        .FirstAsync(c => c.Username.Equals(username));
+                }
+                catch (Exception e)
+                {
+                    throw new Exception("Not found, check credentials");
+                }
+
+                return client;
+            }
+        }
+
+        public async Task<Models.Client.Client> GetClientById(int clientId)
+        {
+            await using (dbCtx = new DbContext())
+            {
+                Models.Client.Client client;
+
+                try
+                {
+                    client = await dbCtx.Clients
+                        .Include(cl => cl.Burials)
+                        .ThenInclude(c => c.BurialPreferences)
+                        .FirstAsync(cl => cl.Id == clientId);
+                }
+                catch (Exception e)
+                {
+                    throw new Exception("Not found, check credentials");
+                }
+
+                return client;
+            }
+        }
+    }
+}
